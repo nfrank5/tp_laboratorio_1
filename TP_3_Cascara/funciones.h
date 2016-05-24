77478eea409b1a8fc30dac85c34b58618fea5fe2
nfrank5 <nfrankenfeld@gmail.com>
2016-05-24 12:03:46 -0300

    subo tercer TP y cascara cuarto TP

--- TP_3_Cascara/funciones.h
+++ TP_3_Cascara/funciones.h
@@ -3,67 +3,21 @@
 #define MAXIMO 5
 
 typedef struct{
-    char titulo[20];
-    char genero[20];
+    char titulo[51];
+    char genero[21];
     int duracion;
-    char descripcion[50];
+    char descripcion[512];
     int puntaje;
-    char linkImagen[50];
+    char linkImagen[151];
     int status;
 }EMovie;
 
-int initializeMovierArrayStatus(EMovie lista[],int length);
+int cargarPeliculaEstructura(EMovie* movie);
+int agregarPelicula(EMovie * movie);
+int borrarPelicula(EMovie * movie);
+void generarPagina(EMovie * movie,char nombre[]);
+int modificarPelicula(EMovie * movie);
 
 
-
-/**
- *  Agrega una pelicula al archivo binario
- *  @param movie la estructura a ser agregada al archivo
- *  @return retorna 1 o 0 de acuerdo a si pudo agregar la pelicula o no
- */
-int agregarPelicula(EMovie movie);
-
-/**
- *  Borra una pelicula del archivo binario
- *  @param movie la estructura a ser eliminada al archivo
- *  @return retorna 1 o 0 de acuerdo a si pudo eliminar la pelicula o no
- */
-int borrarPelicula(EMovie movie);
-
-/**
- *  Genera un archivo html a partir de las peliculas cargadas en el archivo binario.
- *  @param lista la lista de peliculas a ser agregadas en el archivo.
- *  @param nombre el nombre para el archivo.
- */
-void generarPagina(EMovie lista[], char nombre[]);
-
-
-/*
-int initializeCommentArrayStatus(EComments lista[],int length);
-int obtenerEspacioLibre(EUser lista[],int*i,int length);
-int addItem(EUser lista[],EComments commentArray[],int*aux,int length,int lengthComment);
-int findUserByNick(EUser lista[],int*aux,int length);
-int deleteUser(EUser lista[],int*aux,int length);
-int modifyUser(EUser lista[],EComments commentArray[],int*aux,int length,int lengthComment);
-
-
-int obtenerEspacioLibreComments(EComments lista[],int*aux,int length);
-int newComment(EUser lista[],EComments commentArray[],int*aux,int length,int lengthComment);
-int newLike(EComments lista[],int length,EUser userArray[],int lengthUser);
-
-int printInformar(EComments lista[],int length);
-int printUserCommentList(EUser userArray[],EComments commentArray[],int authorArrayLenght,int userArrayLenght);
-
-
-
-
-int findCommentById(EUser book[], EComments author[], int userArrayLenght, int authorArrayLenght, int auxSearch);
-int ordenarLista(EUser lista[],int length);
-int printBookList(EUser lista[],int length);
-int printAuthorList(EComments lista[],int length);
-int imprimirGrafico(EUser lista[],int length);
-int menorMayor(EUser lista[],int length);
-int crearMenu(int* retorno,char items[], int desde, int hasta);
-*/
 
 #endif // FUNCIONES_H_INCLUDED
--- TP_3_Cascara/main.c
+++ TP_3_Cascara/main.c
@@ -3,31 +3,40 @@
 #include "funciones.h"
 #include "utn.h"
 
-
 int main()
 {
-    EMovie movie[MAXIMO];
-    initializeMovierArrayStatus(movie,MAXIMO);
+    EMovie auxMovie;
     char seguir='s';
     int opcion=0;
-
     while(seguir=='s')
     {
-        crearMenu(&opcion, "1- Agregar pelicula\n2- Borrar pelicula\n3- Generar pagina web\n4- Salir\n",1,4);
+        crearMenu(&opcion, "1- Agregar pelicula\n2- Borrar pelicula\n3- Modificar Pelicula\n4- Generar pagina web\n5- Salir\n",1,5);
 
         switch(opcion)
         {
             case 1:
+                system("@cls||clear");
+                if(cargarPeliculaEstructura(&auxMovie)==0)
+                {
+                    agregarPelicula(&auxMovie);
+                }
                 break;
             case 2:
+                system("@cls||clear");
+                borrarPelicula(&auxMovie);
                 break;
             case 3:
-               break;
+                system("@cls||clear");
+                modificarPelicula(&auxMovie);
+                break;
             case 4:
+                system("@cls||clear");
+                generarPagina(&auxMovie,"index.html");
+                break;
+            case 5:
                 seguir = 'n';
                 break;
         }
     }
-
     return 0;
 }
